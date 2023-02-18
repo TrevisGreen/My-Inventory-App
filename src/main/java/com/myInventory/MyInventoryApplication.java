8febed16a2e6d046f661469308404e5c1602497e
TrevisGreen <Trevis.Green@gmail.com>
2023-02-18 11:20:32 -0600

    Just added some space to make it easier to read for me

--- src/main/java/com/myInventory/MyInventoryApplication.java
+++ src/main/java/com/myInventory/MyInventoryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyInventoryApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MyInventoryApplication.class, args);
 	}
 
