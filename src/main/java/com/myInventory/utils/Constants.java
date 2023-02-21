f57b234a25ff1887fc38e7fab07a091dd78562bf
TrevisGreen <Trevis.Green@gmail.com>
2023-02-20 19:06:45 -0600

    Created Constants file

---
+++ src/main/java/com/myInventory/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.myInventory.utils;
+
+public class Constants {
+    // The constant is the user logged in for the current session
+
+    public static final String LOGGED_USER = "loggedUser";
+}
