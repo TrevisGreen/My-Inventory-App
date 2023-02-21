898a40eda31e7bff1c7b079c8a8c8dfb517bbe7b
TrevisGreen <Trevis.Green@gmail.com>
2023-02-21 13:19:14 -0600

    Created InvoiceRepository interface

--- src/main/java/com/myInventory/dao/InvoiceRepository.java
+++ src/main/java/com/myInventory/dao/InvoiceRepository.java
@@ -1,4 +1,8 @@
 package com.myInventory.dao;
 
-public interface InvoiceRepository {
+import com.myInventory.model.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
 }
