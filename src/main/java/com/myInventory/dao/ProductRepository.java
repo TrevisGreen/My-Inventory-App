79b31b5edaedc54ff599ac25ba026ed8c40c8d18
TrevisGreen <Trevis.Green@gmail.com>
2023-02-21 11:59:45 -0600

    Created ProductRepository interface

--- src/main/java/com/myInventory/dao/ProductRepository.java
+++ src/main/java/com/myInventory/dao/ProductRepository.java
@@ -1,4 +1,13 @@
 package com.myInventory.dao;
 
-public interface ProductRepository {
+
+import com.myInventory.model.Product;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    public List<Product> findByManufacturerIgnoreCaseContainingOrModelIgnoreCaseContainingOrDescriptionIgnoreCaseContaining(String manufacturer, String model, String description, Pageable pageable);
 }
