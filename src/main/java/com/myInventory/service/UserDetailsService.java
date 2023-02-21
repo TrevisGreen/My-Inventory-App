171f77584f1a6716a13b4fd90261b7f268052e13
TrevisGreen <Trevis.Green@gmail.com>
2023-02-21 08:15:52 -0600

    Created UserDetailsService class

--- src/main/java/com/myInventory/service/UserDetailsService.java
+++ src/main/java/com/myInventory/service/UserDetailsService.java
@@ -1,4 +1,27 @@
 package com.myInventory.service;
 
-public class UserDetailsService {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    // Connects to the userDao class
+    @Autowired
+    private UserRepository repo;
+
+    /**
+     * method that calls the method 'getUser' in the userDao class
+     * and returns the user's username/email
+     *
+     * @param username
+     * @return UserDetails
+     */
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return repo.findOne(username);
+    }
 }
