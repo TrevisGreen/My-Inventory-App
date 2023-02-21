eb3cd0cde17d7691e9cee71d3607cfe943ee075d
TrevisGreen <Trevis.Green@gmail.com>
2023-02-21 10:21:35 -0600

    Created LoginController class

--- src/main/java/com/myInventory/web/LoginController.java
+++ src/main/java/com/myInventory/web/LoginController.java
@@ -1,4 +1,20 @@
 package com.myInventory.web;
 
+import com.myInventory.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+// Defines the class as a controller in the MVC architecture
+@Controller
 public class LoginController {
+
+    // Initialized first, takes us to the login screen
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "login";
+    }
 }
